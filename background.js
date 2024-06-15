26c52afd7962a9dee3820d32a43b488341d3346a
Vinicius-Marques6 <marquesv473@gmail.com>
2024-06-14 22:20:37 -0400

    YEAH!!

--- background.js
+++ background.js
@@ -1,20 +1,19 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Service worker instalado.');
-    listarCookies();
-  });
-  
-function listarCookies() {
-chrome.cookies.get({ url: "https://x.com", name: "ct0" }, function(cookies) {
-    console.log(cookies);
 });
-}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'getCookies') {
-        chrome.cookies.getAll({ domain: "example.com" }, function(cookies) {
+    if (message.action === 'getCookie') {
+        chrome.cookies.get({ url: "https://x.com", name: "ct0" }, function(cookies) {
             sendResponse({ cookies: cookies });
         });
         return true; // Manter o canal de mensagem aberto para enviar a resposta de forma assíncrona
     }
 });
 
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    console.log('Navigation completed');
+    chrome.tabs.sendMessage(details.tabId, {message: "navigationCompleted"}, function(response) {
+      console.log(response);
+    });
+  }, {url: [{urlMatches: 'https://x.com/*'}]});
--- scripts/yeah.js
+++ scripts/yeah.js
@@ -1,41 +1,158 @@
-console.log('Yeah!!');
-
-window.addEventListener('load', async function() {
-    const cookies = await chrome.cookies.getAll({
-        domain: "https://x.com"
+$( document ).ready(function() {
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.message === "navigationCompleted") {
+            console.log('Navigation completed');
+            fazTudo();
+            sendResponse({ message: "Yeah!" });
+        }
     });
-
-    console.log(cookies)
-
-    $('#react-root').prepend(
-        $(document.createElement("button")).prop({
-            innerHTML: "Click me!"
-        }).on('click', function() {
-            $.ajax({
-                url: "https://x.com/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet",
-                method: 'POST',
-                contentType: "application/json; charset=utf-8",
-                headers: {
-                    "content-type": "application/json",
-                    // coisas
-                },
-                data: JSON.stringify(info)
-            })
-        })
-    )
 });
 
-var info = {
-    "variables": {
-        "tweet_text": "aaa",
+function fazTudo() {
+    setTimeout( async () => {
+        console.log('Yeah!!');
+
+        let ct0;
+
+        await chrome.runtime.sendMessage({ action: 'getCookie' }, function(response) {
+            if (response && response.cookies) {
+                console.log('Cookies recebidos:', response.cookies);
+                ct0 = response.cookies.value;
+            } else {
+                console.log('Nenhum cookie encontrado ou erro ao obter cookies.');
+            }
+        });
+
+        const callback = function(mutationsList, observer) {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    // Verificar se novos nós foram adicionados
+                    if (mutation.addedNodes.length > 0) {
+                        $(mutation.addedNodes).each(function() {
+                            // Realizar a ação desejada com o novo nó usando jQuery
+                            if (this.nodeType === Node.ELEMENT_NODE) {
+                                // Checar se o novo nó contém o seguinte elemento com as respectivas classes
+                                let tweet = $(this).find('.css-175oi2r.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu')
+                                if (tweet.length > 0) {
+                                    let id = $(tweet).find('a').eq(2).attr('href').split('/').pop();
+                                    console.log($(tweet).find('a').eq(2));
+
+                                    if ($(tweet).find('.yeah-div').length > 0) {
+                                        return;
+                                    }
+                                    
+                                    $(tweet).children().last().children().children().children().eq(2).after(
+                                        $(document.createElement("div")).prop({
+                                            class: "css-175oi2r r-18u37iz r-1h0z5md r-13awgt0 yeah-div"
+                                        }).append(
+                                            $(document.createElement("button")).prop({
+                                                class: "css-175oi2r r-1777fci r-bt1l66 r-bztko3 r-lrvibr r-1loqt21 r-1ny4l3l r-37j5jr",
+                                                style: "text-overflow: unset; color: rgb(113, 118, 123);",
+                                                innerHTML: "Yeah!"
+                                            }).hover(
+                                                function() {
+                                                    // on mouseover
+                                                    $(this).css('color', 'rgb(0, 186, 124)'); // change color to green
+                                                }, 
+                                                function() {
+                                                    // on mouseout
+                                                    $(this).css('color', 'rgb(113, 118, 123)'); // change color back to original
+                                                }
+                                            ).on('click', function() {
+                                                $.ajax({
+                                                    url: "https://x.com/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet",
+                                                    method: 'POST',
+                                                    contentType: "application/json; charset=utf-8",
+                                                    headers: {
+                                                        "content-type": "application/json",
+                                                        "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+                                                        "x-csrf-token": ct0,
+                                                    },
+                                                    data: JSON.stringify(getMessage(id)),
+                                                })
+                                            })
+                                        )
+                                    )
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        };
+
+        // Criar um novo observer
+        const observer = new MutationObserver(callback);
+
+        // Selecionar o nó do DOM que você quer observar
+        const targetNode = document.querySelector('body');
+
+        if (!targetNode) {
+            console.error('Nó não encontrado.');
+        }
+
+        // Configurar as opções do observer
+        const config = {
+            childList: true, // Observar a adição ou remoção de nós filhos
+            subtree: true    // Observar também os nós descendentes
+        };
+
+        // Iniciar o observer
+        observer.observe(targetNode, config);
+
+        // $('#react-root').prepend(
+        //     $(document.createElement("button")).prop({
+        //         innerHTML: "Click me!"
+        //     }).on('click', function() {
+        //         $.ajax({
+        //             url: "https://upload.x.com/i/media/upload.json?command=INIT&total_bytes=31433&media_type=image%2Fjpeg&media_category=tweet_image",
+        //             method: 'POST',
+        //             headers: {
+        //                 "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+        //                 "x-csrf-token": ct0,
+        //             }
+        //         }).done((data) => {
+        //             data = JSON.parse(data);
+        //             console.log(data);
+        //             // $.ajax({
+        //             //     url: `https://upload.x.com/i/media/upload.json?command=APPEND&media_id=${data.media_id}&segment_index=0`
+        //             // })
+        //         })
+
+
+        //         // $.ajax({
+        //         //     url: "https://x.com/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet",
+        //         //     method: 'POST',
+        //         //     contentType: "application/json; charset=utf-8",
+        //         //     headers: {
+        //         //         "content-type": "application/json",
+        //         //         "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+        //         //         "x-csrf-token": ct0,
+        //         //     },
+        //         //     data: JSON.stringify(getMessage("1801625496733552760")),
+        //         // })
+        //     })
+        // )
+    }, 1000);
+}
+
+function getMessage(id) {
+    return {
+        "variables": {
+        "tweet_text": "",
         "reply": {
-            "in_reply_to_tweet_id": "1801625496733552760",
+            "in_reply_to_tweet_id": id,
             "exclude_reply_user_ids": []
         },
         "batch_compose": "BatchSubsequent",
         "dark_request": false,
         "media": {
-            "media_entities": [],
+            "media_entities": [
+                {
+                    "media_id": "1801793988610895872",
+                    "tagged_users": []
+                }
+            ],
             "possibly_sensitive": false
         },
         "semantic_annotation_ids": []
@@ -66,4 +183,5 @@ var info = {
         "responsive_web_enhance_cards_enabled": false
     },
     "queryId": "oB-5XsHNAbjvARJEc8CZFw"
+    }
 }
